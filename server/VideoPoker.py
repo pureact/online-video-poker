7a49566585c48fb3cc9e4ef6249bca01b867c521
NAEK2K <naek2k@outlook.com>
2021-02-27 22:22:56 -0500

    migrating to /server

--- server/VideoPoker.py
+++ server/VideoPoker.py
@@ -9,25 +9,29 @@ class Card:
     def print(self):
         print("{} {}".format(self.face_value, self.suit))
 
+
 class Deck:
     def __init__(self):
         num_face_values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
         royal_face_values = ["J", "Q", "K", "A"]
         suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
 
-        self.deck = [Card(num, suit) for suit in suits for num in num_face_values] + [Card(num, suit) for suit in suits for num in royal_face_values]
+        self.deck = [Card(num, suit) for suit in suits for num in num_face_values] + [
+            Card(num, suit) for suit in suits for num in royal_face_values
+        ]
 
     def reset_deck(self):
         self.__init__()
 
     def get_card(self):
-        card_index = randint(0, len(self.deck)-1)
+        card_index = randint(0, len(self.deck) - 1)
 
         return self.deck.pop(card_index)
 
     def print(self):
         for card in self.deck:
             card.print()
+
 
 class Hand:
     def __init__(self, initial_hand):
@@ -45,7 +49,7 @@ class Hand:
     def reset_hand(self, new_hand):
         self.__init__(new_hand)
 
-    def get_replaced_cards():
+    def get_replaced_cards(self):
         return list(self.replaced_cards)
 
     def print(self):
