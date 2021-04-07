16d94357c7ec1d646e3a3c0c89888d1a237da29c
vinhvn <vinhhnguyen@cmail.carleton.ca>
2021-04-07 15:01:08 -0400

    chore: ⚙️ layout for card, table components

---
+++ app/src/components/Table.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type TableProps = {
+  bet: number, // or col, or index. feel free to change it
+}
+
+function Table({ bet }: TableProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Table
