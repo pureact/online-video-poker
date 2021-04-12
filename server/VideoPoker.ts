b0960ac576b632c7b1120cf411aca31370ecb7ae
Matthew <matthewjo30@gmail.com>
2021-04-11 20:49:56 -0400

    game should work

--- server/VideoPoker.ts
+++ server/VideoPoker.ts
@@ -22,12 +22,24 @@ class VideoPoker {
             this.hand.push(this.get_card())
         }
 
-        // royal flush //this.hand = [{face_value: "10", suit: "H"}, {face_value: "14", suit: "H"}, {face_value: "11", suit: "H"}, {face_value: "12", suit: "H"}, {face_value: "13", suit: "H"}];
-        // straight flush //this.hand = [{face_value: "5", suit: "H"}, {face_value: "6", suit: "H"}, {face_value: "8", suit: "H"}, {face_value: "7", suit: "H"}, {face_value: "9", suit: "H"}];
-        //this.hand = [{face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}, {face_value: "9", suit: "H"}];
-        //this.hand = [{face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}, {face_value: "5", suit: "H"}];
-        //console.log(this.hand);
-
+        // royal flush 
+        //this.hand = [{face_value: "10", suit: "H"}, {face_value: "14", suit: "H"}, {face_value: "11", suit: "H"}, {face_value: "12", suit: "H"}, {face_value: "13", suit: "H"}];
+        // straight flush 
+        //this.hand = [{face_value: "5", suit: "H"}, {face_value: "6", suit: "H"}, {face_value: "8", suit: "H"}, {face_value: "7", suit: "H"}, {face_value: "9", suit: "H"}];
+        // four of a kind
+        //this.hand = [{face_value: "11", suit: "H"}, {face_value: "11", suit: "D"}, {face_value: "11", suit: "C"}, {face_value: "11", suit: "S"}, {face_value: "7", suit: "D"}];
+        // full house
+        //this.hand = [{face_value: "10", suit: "H"}, {face_value: "10", suit: "D"}, {face_value: "10", suit: "S"}, {face_value: "9", suit: "C"}, {face_value: "9", suit: "D"}];
+        // Flush
+        //this.hand = [{face_value: "4", suit: "S"}, {face_value: "11", suit: "S"}, {face_value: "8", suit: "S"}, {face_value: "2", suit: "S"}, {face_value: "9", suit: "S"}];
+        // Straight
+        //this.hand = [{face_value: "9", suit: "C"}, {face_value: "8", suit: "D"}, {face_value: "7", suit: "S"}, {face_value: "6", suit: "D"}, {face_value: "5", suit: "H"}];
+        // Three of a kind
+        //this.hand = [{face_value: "7", suit: "C"}, {face_value: "7", suit: "D"}, {face_value: "7", suit: "S"}, {face_value: "13", suit: "C"}, {face_value: "3", suit: "D"}];
+        // Two pair
+        //this.hand = [{face_value: "4", suit: "C"}, {face_value: "4", suit: "S"}, {face_value: "3", suit: "C"}, {face_value: "3", suit: "D"}, {face_value: "12", suit: "C"}];
+        // Pair
+        //this.hand = [{face_value: "14", suit: "H"}, {face_value: "14", suit: "D"}, {face_value: "8", suit: "C"}, {face_value: "4", suit: "S"}, {face_value: "7", suit: "H"}];
     }
 
     generate_deck(): Card[] {
@@ -70,7 +82,6 @@ class VideoPoker {
         hand.suits = [...new Set(hand.suits)]
         let royal_faces: string[] = ["10", "11", "12", "13", "14"]
         if (royal_faces.join("") == hand.face_values.join("") && hand.suits.length == 1) {
-            console.log("test");
             return "Royal Flush"
         }
         return ""
@@ -80,9 +91,6 @@ class VideoPoker {
         let hand = this.get_hand();
         hand.face_values.sort();
         hand.suits = [...new Set(hand.suits)];
-        console.log(hand.suits);
-        console.log(hand.face_values);
-        console.log(parseInt("5"));
 
         for (let i = 0; i < hand.face_values.length - 1; i++) {
             if (1 + parseInt(hand.face_values[i]) != parseInt(hand.face_values[i + 1])) {
@@ -93,15 +101,22 @@ class VideoPoker {
     }
 
     is_four_of_a_kind(): string {
+
         let hand = this.get_hand();
-        let count = 1;
-        hand.face_values.sort();
-        for (let i = 0; i < hand.face_values.length - 1; i++) {
-            if (hand.face_values[i] == hand.face_values[i + 1]) {
-                count++;
+        let face_counts: Record<string, number> = {}
+        for(let i = 0; i < hand.face_values.length; i++){
+            if(hand.face_values[i] in face_counts){
+                face_counts[hand.face_values[i]] += 1;
+                if(face_counts[hand.face_values[i]] == 4){
+                    return "Four of a Kind";
+                }
+            }
+            else if(!(hand.face_values[i] in face_counts)){
+                face_counts[hand.face_values[i]] = 1;
             }
         }
-        return count >= 4 ? "Four of a Kind" : "";
+
+        return "";
     }
 
     is_full_house(): string {
@@ -151,11 +166,19 @@ class VideoPoker {
 
     is_jacks_or_better(): string {
         let hand = this.get_hand();
-        for (let i = 0; i < hand.face_values.length; i++) {
-            if (parseInt(hand.face_values[i]) > 10) {
-                return "Jacks or Better";
+        let face_counts: Record<string, number> = {}
+        for(let i = 0; i < hand.face_values.length; i++){
+            if(hand.face_values[i] in face_counts){
+                face_counts[hand.face_values[i]] += 1;
+                if(face_counts[hand.face_values[i]] == 2 && parseInt(hand.face_values[i]) > 10){
+                    return "Jacks or Better";
+                }
+            }
+            else if(!(hand.face_values[i] in face_counts)){
+                face_counts[hand.face_values[i]] = 1;
             }
         }
+
         return "";
     }
 
